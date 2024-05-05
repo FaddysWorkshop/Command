1144ed87b7035de6d412c9e1432e94727c0cf42f
Faddy Michel Samaan <faddy.michel@gmail.com>
2024-05-05 12:02:06 -0400

    @faddymichel: On branch main
    *	deleted:    example.js
    *	new file:   examples/hello.js
    *	new file:   examples/writingToFile.js

---
+++ examples/hello.js
@@ -0,0 +1,21 @@
+/*
+# Example: Hello World!
+
+The traditional "Hello World" example using Faddy's Command.
+
+*/
+
+import command from '@faddys/command';
+
+// Create the command function:
+const echo = await command ( `echo "Hello World! This is Faddy's Command in Solidarity with The People of Palestine till The Whole Land is FREE!"` );
+
+// Call the command function to retrieve the output by passing the symbol returned from `Symbol .for ( 'output' )` as the first parameter:
+const output = await echo ( Symbol .for ( 'output' ) );
+
+// The output is an array containing the lines printed to the standard output of the command.
+// Since the output contains only one line, pop it from the array and log it to the console:
+console .log ( output .pop () );
+
+// The following will be printed:
+// Hello World! This is Faddy's Command in Solidarity with The People of Palestine till The Whole Land is FREE!
---
+++ examples/writingToFile.js
@@ -0,0 +1,6 @@
+import command from '@faddys/command';
+
+const write = await command ( 'cat - > faddys.scratch' );
+
+write ( "Hello World! This is Faddy's Command in Solidarity with The People of Palestine till The Whole Land is FREE!" );
+write ( Symbol .for ( 'end' ) );
